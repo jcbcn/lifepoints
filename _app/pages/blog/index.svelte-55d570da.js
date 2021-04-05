f8b8ee6d4299834aef628414b4958e60fb44bfbc
jcbcn <jcbcn@users.noreply.github.com>
2021-04-05 21:22:18 +0000

    Deploying to gh-pages from @ jcbcn/lifepoints@09c3bdb0ac58e3ec912678982e44e6cf9699499e 🚀

---
+++ _app/pages/blog/index.svelte-55d570da.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as o,e as r,t,c as e,a as n,b as c,d,f as l,g as u,n as i}from"../../chunks/vendor-2525ad3d.js";function f(s){let a,o;return{c(){a=r("div"),o=t("I'm blog!")},l(s){a=e(s,"DIV",{});var r=n(a);o=c(r,"I'm blog!"),r.forEach(d)},m(s,r){l(s,a,r),u(a,o)},p:i,i:i,o:i,d(s){s&&d(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,f,o,{})}}
