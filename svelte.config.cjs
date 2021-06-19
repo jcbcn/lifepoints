0e31b46bd71dbc4d00a669e81fb3997427ebda87
Jacob <jcbcn@users.noreply.github.com>
2021-06-19 14:00:44 +0100

    Revert svelte config

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,6 @@
-import sveltePreprocess from 'svelte-preprocess'
-import static from '@sveltejs/adapter-static'
-import pkg from `./package.json`
+const sveltePreprocess = require('svelte-preprocess');
+const static = require('@sveltejs/adapter-static');
+const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
