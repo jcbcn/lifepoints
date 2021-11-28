56ba9b03662fffe6fb9da57fe11c59422e709f6e
jcbcn <jcbcn@users.noreply.github.com>
2021-11-28 19:59:56 +0000

    Initial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,12 @@
-const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
+const config = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
 
-module.exports = {
-	mode: "jit",
-	purge: {
-		content: [
-			"./src/**/*.{html,js,svelte,ts}",
-		],
-		options: {
-			defaultExtractor: (content) => [
-				// If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
-				...tailwindExtractor(content),
-				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
-				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
-			],
-			keyframes: true,
-		},
-	},
 	theme: {
-		extend: {},
+		extend: {}
 	},
-	variants: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms')
-	],
+
+	plugins: []
 };
+
+module.exports = config;
